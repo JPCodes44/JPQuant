c40e2d9f7a5044d41108cfb35e23e9330eee3d83
Justin Mak <jp.mak44@gmail.com>
2025-03-04 18:44:48 -0500

    added external strats and indicators

--- indicators_26.py
+++ indicators_26.py
@@ -21,5 +21,12 @@ def trend_lines(df):
         slope, intercept, r_value, p_value, std_err = linregress(x=data1['date_id'].values.tolist(), y=data1['Low'].values.tolist())
         data1['Low'] = data1.loc(data1['Low'] < slope * data1['date_id']+intercept)
         
-    bot_slope, intercept, r_value, p_value, std_err = linregress(x=data1['date_id'].values.tolist(), y=data1['Low'].values.tolist())
-    data0['Low'] = data1.loc(data1['Low'] < slope * data1['date_id']+intercept)
+    bot_slope, intercept, r_value, p_value, std_err = linregress(x=data1['date_id'].values.tolist(), y=data1['Close'].values.tolist())
+    data0['low_trend'] = bot_slope * data0['date_id'] + intercept
+    
+    # get the amount of times the price has touched the trend
+    data0['touched_top'] = ((data0['Open'] < data0['high_trend']) & (data0['Close'] < data0['high_trend']) & (data0['High'] > data0['high_trend']))
+    data0['touched_bot'] = ((data0['Open'] > data0['low_trend']) & (data0['Close'] > data0['low_trend']) & (data0['Low'] < data0['low_trend']))
+    
+    return data0[['high_tend', 'low_trend', 'touched_top', 'touched_bot']]
+    
--- strats_26.py
+++ strats_26.py
@@ -1,1 +1,44 @@
-from backtesting import Strategy
+# get data on the 15min timeframe
+
+from backtesting import Backtest
+from datetime import datetime
+import warnings
+
+warnings.filterwarnings("ignore")
+import pandas as pd
+from strats_26 import *
+from dataset import get_candles_since_date
+
+
+def run_backtest(stratName, symbol, timeframe, start_time, plot=True, save_data=True):
+    # data = pd.read_csv('feb23/BTC-USD-15m_filled.csv', index_col='datetime', parse_dates=True)
+    data = get_candles_since_date(
+        symbol, timeframe=timeframe, start_time=start_time, save_data=save_data
+    )
+
+    data.columns = ["Open", "High", "Low", "Close", "Volume"]
+
+    # run the backtest
+    bt = Backtest(
+        data, eval(stratName), cash=10000, commission=0.001, exclusive_orders=True
+    )
+
+    # get the results
+    output = bt.run()
+    print(output)
+
+    # plot the results
+    if plot:
+        bt.plot()
+
+    print(data.head())
+
+
+run_backtest(
+    "FlagCont",
+    "ETH-USD",
+    timeframe="1h",
+    start_time=datetime(2022, 1, 1, 12, 0),
+    plot=True,
+    save_data=False,
+)
